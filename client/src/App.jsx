c4c47fd647bbc34a580a5c14faaad7244130bb8f
Aishwarya <aishwaryazingade292@gmail.com>
2024-06-18 16:44:44 +0530

    create pages ad route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 
 function App() {
@@ -7,7 +13,17 @@ function App() {
 
   return (
     <>
-       <h1 className='text-red-950'>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/sign-in' element={<Signin/>}></Route>
+          <Route path='/sign-up' element={<Signup/>}></Route>
+          <Route path='/dashboard' element={<Dashboard/>}></Route>
+          <Route path='/projects' element={<Projects/>}></Route>
+ 
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
